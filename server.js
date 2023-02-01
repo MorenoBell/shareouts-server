4cbdf0eec5aed9dcb4e9f4dd8996a86c2c2d6656
Moreno Bellini <morenobellini15@gmail.com>
2023-02-01 21:24:50 +0100

    asdasdasd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./Router')
+const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const uri = `mongodb+srv://morenobellini15:${process.env.MONGO_PW}@shareoutsdb.8jadq8f.mongodb.net/?retryWrites=true&w=majority`;
@@ -15,6 +16,7 @@ async function connect() {
   }
 }
 connect();
+app.use(cors())
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");//"https://todoapp-ouu6.onrender.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
