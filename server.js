d90682a62bb0653206ddc8fe5c453cf04e373ea1
Moreno Bellini <morenobellini15@gmail.com>
2023-02-01 22:38:25 +0100

    try fix cors

--- server.js
+++ server.js
@@ -16,11 +16,7 @@ async function connect() {
   }
 }
 connect();
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 app.use(cors())
 const port = 5000;
 app.listen(port, () => console.log("server started on port" + port));
