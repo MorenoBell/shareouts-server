bb2e8ef6838b542c35c166a4febc3bf404f3931c
Moreno Bellini <morenobellini15@gmail.com>
2023-02-01 22:08:36 +0100

    cors fix

--- server.js
+++ server.js
@@ -16,12 +16,19 @@ async function connect() {
   }
 }
 connect();
-app.use(cors())
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");//"https://todoapp-ouu6.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
+app.use(cors())
 const port = 5000;
 app.listen(port, () => console.log("server started on port" + port));
 app.use(express.json())
